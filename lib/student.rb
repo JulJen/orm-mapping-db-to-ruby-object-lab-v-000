371565db1439792985bc8bf76364b5ee7e99384c
Jul Jen <juliajen.prog@gmail.com>
2018-03-21 15:37:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,14 +15,15 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    # sql is a variable that holds data for all students returned into a database
     sql = <<-SQL
-      SELECT * 
-      FROM students 
+      SELECT *
+      FROM students
     SQL
 
-    DB[:conn].execute(sql).map do |row| #value of the hash(call to databse) is a new instance of the SQLite3::Database class
+    DB[:conn].execute(sql).map do |row| #value of the hash(call to database) is a new instance of the SQLite3::Database class- iterate over each row and use the self.new_from_db method to create a new Ruby object for each row.
       self.new_from_db(row)
-    
+
   end
 
   def self.find_by_name(name)
