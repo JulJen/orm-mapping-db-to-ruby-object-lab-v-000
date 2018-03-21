d9f4e951772bcf7d8da53b168ebf444e73d8fb5b
Jul Jen <juliajen.prog@gmail.com>
2018-03-21 18:26:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -129,14 +129,14 @@ end
   end
 
 
-  def self.first_student_in_grade_10
+  def self.all_students_in_grade_X(num)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
+      WHERE grade = ?
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
