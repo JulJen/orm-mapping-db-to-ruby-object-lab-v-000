67cead1a2705cce3afdd3e48cef0d11d1621064e
Jul Jen <juliajen.prog@gmail.com>
2018-03-21 18:26:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -129,16 +129,16 @@ end
   end
 
 
-  def self.all_students_in_grade_X(num)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = ?
-    SQL
+  # def self.all_students_in_grade_X(num)
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM students
+  #     WHERE grade = ?
+  #   SQL
+  #
+  #   DB[:conn].execute(sql, num).map do |row|
+  #     self.new_from_db(row)
+  #   end
+  # end
 
-    DB[:conn].execute(sql, num).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
-end 
+end
