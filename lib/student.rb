de8785bd77a3f6c3c61049cf5f578dd3d8236178
Jul Jen <juliajen.prog@gmail.com>
2018-03-21 17:43:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,9 +43,9 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(students.name)
+      SELECT COUNT(students.name) as name
       FROM students
-      GROUP BY grade WHERE grade = 9
+      GROUP BY name WHERE grade = 9
     SQL
   end
 
